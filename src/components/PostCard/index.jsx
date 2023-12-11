ca6c4b929e200139a90194ea2bd2e14444ff528e
RODRIGO CASTRO <rodrigobfcastro@gmail.com>
2023-12-11 19:59:08 -0300

    FirstVersion

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+export const PostCard = ({ title, cover, body }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  </div>
+);
